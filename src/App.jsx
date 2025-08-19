8db04a981dde98339babbd513e838dc8f1ac99f2
Allan Sechrist <sechristallan@gmail.com>
2025-08-19 09:24:50 +0900

    Get data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
+import { useState, useEffect } from 'react';
+const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
 const App = () => {
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true); // used to detect if data is loading still
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      try {
+        const res = await fetch(API_URL);
+        if(!res.ok) throw new Error("Failed to fetch data");
+        const data = await res.json();
+        console.log(data);
+        setCoins(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchCoins();
+  }, [])
+
   return (
     <div>
       <h1>Crypto Dash</h1>
